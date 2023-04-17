e27188753570b98b07fc7fe894b3cb5ff0b33159
Keiron McCammon <keiron@mccammon.me>
2023-04-17 15:02:11 -0400

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 One Handed Ventures, LLC
+ */
+
+/**
+ * @file app.ts
+ * @description The main entry point for the application.
+ */
+export default function app() {
+  console.log('Hello, world!');
+}
+
+app();
